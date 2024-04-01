d682653cd07b27b5f7944828da0af3960a071e7a
Yefim Butrameev <yefim.butrameev@gmail.com>
2024-04-01 15:42:33 +0300

    Fix address to monitor var typo

--- solana/config.js
+++ solana/config.js
@@ -5,6 +5,7 @@ const { Keypair, Connection } = require("@solana/web3.js");
 const OWNER_SECRET_KEY = process.env.OWNER_SECRET_KEY.split(',');
 const WALLET = Keypair.fromSecretKey(new Uint8Array(OWNER_SECRET_KEY));
 const ADDRESS_TO_MONITOR = process.env.ADDRESS_TO_MONITOR;
+console.log(ADDRESS_TO_MONITOR)
 const RPC_ENDPOINT = process.env.RPC_ENDPOINT;
 const CONNECTION = new Connection(RPC_ENDPOINT, "confirmed");
 const BATCH_SIZE = 40;
